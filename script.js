992c3cee4223311a4219b9854f9e1d7f296abbbc
Aditya Bikram Mitra <54503735+aditya083-etce@users.noreply.github.com>
2021-05-20 17:59:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").style.fontSize = "4em";
+document.querySelector("h1").style.background ="violet";
+document.querySelector("ul").style.fontSize = "2em";
+document.getElementsByClassName("first")[0].style.fontSize = "3em";
+document.querySelector("span").style.fontSize = "2rem";
+document.querySelector("span").style.background ="orange";
+document.getElementById("games").style.background = "yellow";
+document.getElementsByClassName("game")[0].style.background = "#418383";
+document.getElementsByClassName("game")[1].style.background = "#B14C3C";
+document.getElementsByClassName("game")[2].style.background = "#FEFE2A";
+document.getElementsByClassName("game")[3].style.background = "#FE2A2A";
+document.getElementsByClassName("game")[4].style.background = "#08B3B3";
+document.getElementsByClassName("game")[5].style.background = "#DAA520";
+
+document.querySelector("h1").className = "coolTitle";
+
+document.querySelector("button").className = "btn-custom";
